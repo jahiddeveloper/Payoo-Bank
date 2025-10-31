91b2f2ca47fd00afa9141c0d4b390cc6b123fda9
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-10-31 12:49:30 +0600

    cashout, transfer money, get bounos, pay bill is compleat

--- home.js
+++ home.js
@@ -9,14 +9,14 @@ document.getElementById("add-money-btn")
         let addAmount = parseInt(document.getElementById("add-amount").value);
         let pinNumber = document.getElementById("add-pin").value;
 
-        let availableBalance = parseInt(document.getElementById("available-balance").innerText); 
+        let availableBalance = parseInt(document.getElementById("available-balance").innerText);
 
-        if(accountNumber.length < 11) {
+        if (accountNumber.length < 11) {
             alert("Please provid a vlaid account number.")
             return;
         }
 
-        if(pinNumber != vlaidPin) {
+        if (pinNumber != vlaidPin) {
             alert("Please provide a valid pin number.")
             return;
         }
@@ -27,4 +27,190 @@ document.getElementById("add-money-btn")
         let addAmount2 = document.getElementById("add-amount");
         addAmount2.value = "";
 
+    })
+
+// feature toggleing
+
+// add money button
+
+document.getElementById("add-money")
+    .addEventListener('click', function () {
+
+        let forms = document.getElementsByClassName("form")
+
+        for (let form of forms) {
+            form.style.display = "none";
+        }
+        document.getElementById("add-money-form").style.display = "block";
+    })
+
+// cash out button
+
+document.getElementById("cashout")
+    .addEventListener('click', function () {
+
+        let forms = document.getElementsByClassName("form")
+
+        for (let form of forms) {
+            form.style.display = "none";
+        }
+        document.getElementById("cashout-form").style.display = "block";
+    })
+
+// transfer money button
+
+document.getElementById("transfer-money")
+    .addEventListener('click', function () {
+
+        let forms = document.getElementsByClassName("form")
+
+        for (let form of forms) {
+            form.style.display = "none";
+        }
+        document.getElementById("transfer-money-form").style.display = "block";
+    })
+
+// get bousos
+
+document.getElementById("get-bounos")
+    .addEventListener('click', function () {
+
+        let forms = document.getElementsByClassName("form")
+
+        for (let form of forms) {
+            form.style.display = "none";
+        }
+        document.getElementById("bonus-form").style.display = "block";
+    })
+
+// pay bill
+
+document.getElementById("pay-bill")
+    .addEventListener('click', function () {
+
+        let forms = document.getElementsByClassName("form")
+        for (let form of forms) {
+            form.style.display = "none";
+        }
+        document.getElementById("bill-form").style.display = "block";
+    })
+
+// withdraw btn
+
+document.getElementById("withdraw-money-btn")
+    .addEventListener('click', function (e) {
+        e.preventDefault();
+
+
+        let agentNumber = document.getElementById("agent-number").value;
+        let amount = parseInt(document.getElementById("amount").value);
+        let pin = document.getElementById("pin").value;
+
+        let availableBalance = parseInt(document.getElementById("available-balance").innerText);
+
+        if (agentNumber.length < 11) {
+            alert("Please provid valid agent number.")
+            return;
+        }
+
+        if (pin != vlaidPin) {
+            alert("Please Provid a valid pin number.");
+            return;
+        }
+
+        let newAvailableBalance = availableBalance - amount;
+        document.getElementById("available-balance").innerText = newAvailableBalance;
+
+        let amount2 = document.getElementById("amount")
+        amount2.value = "";
+    })
+
+// Transfer btn
+
+document.getElementById("transfer-money-btn")
+    .addEventListener('click', function (e) {
+        e.preventDefault();
+
+
+        let userNumber = document.getElementById("user-number").value;
+        let transferAmount = parseInt(document.getElementById("transfer-amount").value);
+        let transferPin = document.getElementById("transfer-pin").value;
+
+        let availableBalance = document.getElementById("available-balance").innerText;
+
+        if (userNumber.length < 11) {
+            alert("Please Provid valid user number.")
+            return;
+        }
+        if (transferPin != vlaidPin) {
+            alert("Please provid a valid pin number.")
+            return;
+        }
+
+        let newAvailableBalance = availableBalance - transferAmount;
+        document.getElementById("available-balance").innerText = newAvailableBalance;
+
+
+    })
+
+// get bounon btn
+
+document.getElementById("get-bounos-btn")
+    .addEventListener('click', function (e) {
+        e.preventDefault();
+
+        let bounos = parseInt(200412);
+
+        let bounosCopon = parseInt(document.getElementById("bonus-coupon").value);
+        let availableBalance = parseInt(document.getElementById("available-balance").innerText);
+        let bounosBanlance = parseInt(20);
+
+        let newAvailableBalance = availableBalance + bounosBanlance;
+
+        if (bounos == bounosCopon) {
+            document.getElementById("available-balance").innerText = newAvailableBalance;
+
+            let bounosCopon2 = document.getElementById("bonus-coupon")
+            bounosCopon2.value = "";
+
+            alert("Add $20 in your account.");
+            return;
+        }
+        else {
+            alert("Please write a valid copun code.")
+
+            let bounosCopon2 = document.getElementById("bonus-coupon")
+            bounosCopon2.value = "";
+            return;
+        }
+
+    })
+
+    // bill btn
+
+    document.getElementById("bill-btn")
+    .addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        let bill = document.getElementById("bill").value;
+        let billerAccountNumber = document.getElementById("biller-account-number").value;
+        let billAmount = parseInt(document.getElementById("bill-amount").value);
+        let billPin = document.getElementById("bill-pin").value;
+
+        let availableBalance = parseInt(document.getElementById("available-balance").innerText);
+
+        if(billerAccountNumber.length < 11) {
+            alert("Please provid a valid biller account number.")
+            return;
+        }
+
+        if(billPin != vlaidPin) {
+            alert("Please provid a valid pin number.")
+        }
+
+        let newAvailableBalance = availableBalance - billAmount;
+        document.getElementById("available-balance").innerText = newAvailableBalance;
+
+        let billAmount2 = document.getElementById("bill-amount");
+        billAmount2.value = "";
     })
